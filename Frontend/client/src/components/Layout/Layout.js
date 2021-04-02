b2bdfe09c627a9865beeae3b1720b8602fe742c2
rarcz29 <rafalcz29@gmail.com>
2021-04-02 14:59:27 +0200

    Add a cover image to the home page

--- Frontend/client/src/components/Layout/Layout.js
+++ Frontend/client/src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
+import backgroundImage from 'assets/home-background.jpg';
 import Header from 'components/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { HomeImage } from './styles';
 
 const Layout = () => {
     return (
         <>
             <Header />
+            <HomeImage src={backgroundImage} />
             <main>
                 <Outlet />
             </main>
---
+++ Frontend/client/src/components/Layout/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const HomeImage = styled.img`
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+`;
