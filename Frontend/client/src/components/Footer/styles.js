063227f09a4adfdb8d05f0130b83cc107eb769df
rarcz29 <rafalcz29@gmail.com>
2021-04-09 21:56:29 +0200

    Create a login page

--- Frontend/client/src/components/Footer/styles.js
+++ Frontend/client/src/components/Footer/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const StyledFooter = styled.footer`
     width: 100%;
-    height: 400px;
+    height: 70vh;
 `;
--- Frontend/client/src/views/LoginView/LoginView.js
+++ Frontend/client/src/views/LoginView/LoginView.js
@@ -1,7 +1,35 @@
+import Button from 'components/common/Button';
+import TextInput from 'components/common/TextInput';
 import React from 'react';
+import { Container, LinksWrapper, StyledForm } from './styles';
 
 const LoginView = () => {
-    return <div>Login View</div>;
+    return (
+        <Container>
+            <h1>Log in</h1>
+            <StyledForm>
+                <TextInput
+                    width="100%"
+                    height="47px"
+                    backgroundColor="#EFEFEF"
+                    placeholder="Email..."
+                ></TextInput>
+                <TextInput
+                    width="100%"
+                    height="47px"
+                    backgroundColor="#EFEFEF"
+                    placeholder="Password..."
+                ></TextInput>
+                <Button width="100%" height="47px">
+                    Log in
+                </Button>
+            </StyledForm>
+            <LinksWrapper>
+                <a>Forgot password?</a>
+                <a>Don't have an account?</a>
+            </LinksWrapper>
+        </Container>
+    );
 };
 
 export default LoginView;
