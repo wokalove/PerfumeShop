81a0cdcdd2ac362dc7fa9b2d529b9e9547a8bf74
PrzemekBarczyk <przemek.barczyk@gmail.com>
2021-04-14 12:26:19 +0200

    Reverted modification of TransactionRepository

--- Backend/server/src/Repository/TransactionRepository.php
+++ Backend/server/src/Repository/TransactionRepository.php
@@ -19,45 +19,6 @@ class TransactionRepository extends ServiceEntityRepository
         parent::__construct($registry, Transaction::class);
     }
 
-    public function addTransaction(Transaction $transaction)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($transaction);
-        $entityManager->flush;
-    }
-
-    public function deleteTransaction(Transaction $transaction)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->remove($transaction);
-        $entityManager->flush();
-    }
-
-    public function getTransactionById(int $id): ?Transaction
-    {
-        $transaction = $this->getDoctrine()->getRepository(Transaction::class)->find($id);
-        return $transaction;
-    }
-
-    public function getTransactions(int $limit): Array
-    {
-        $conn = $this->getEntityManager()->getConnection();
-
-        // TODO: add limit to query
-        $sql = '
-            SELECT DISTINCT * FROM transaction
-            ';
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAllAssociative();
-    }
-
-    public function getAllTransactions(): Array
-    {
-        $transaction = $this->getDoctrine()->getRepository(Transaction::class)->findAll();
-        return $transaction;
-    }
-
     // /**
     //  * @return Transaction[] Returns an array of Transaction objects
     //  */
