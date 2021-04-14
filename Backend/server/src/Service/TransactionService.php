95a516d4cf69967f9df5f84fd3c370d395cc05d7
PrzemekBarczyk <przemek.barczyk@gmail.com>
2021-04-14 12:26:31 +0200

    Modified TransactionService
    Added more methods which now use doctrine.

--- Backend/server/src/Service/TransactionService.php
+++ Backend/server/src/Service/TransactionService.php
@@ -1,29 +1,42 @@
 <?php
 namespace App\Service;
 
-use App\Repository\TransactionRepository;
 use App\Entity\Transaction;
 use Doctrine\ORM\EntityManagerInterface;
 
 class TransactionService
 {
-    protected TransactionRepository $transactionRepository;
+    private EntityManagerInterface $em;
 
-    /**
-     * AuthorizationService constructor.
-     */
-    public function __construct()
+    public function __construct(EntityManagerInterface $em)
     {
-        $this->transactionRepository = new TransactionRepository();
+        $this->em = $em;
     }
 
-    public function getTransactions(int $limit): Array
+    public function addTransaction(Transaction $transaction)
     {
-        return $this->transactionRepository->getTransactions($limit);
+        $this->em->persist($transaction);
+        $this->em->flush();
     }
 
-    public function getAllTransactions(): Array
+    public function deleteTransaction(Transaction $transaction)
     {
-        return $this->transactionRepository->getAllTransactions();
+        $this->em->remove($transaction);
+        $this->em->flush();
+    }
+
+    public function getTransactionById(int $id): ?Transaction
+    {
+        return $this->em->getRepository(Transaction::class)->find($id);
+    }
+
+    public function getTransactions(int $limit)
+    {
+        return $this->em->getRepository(Transaction::class)->findBy(null, null, $limit);
+    }
+
+    public function getAllTransactions()
+    {
+        return $this->em->getRepository(Transaction::class)->findAll();
     }
 }
