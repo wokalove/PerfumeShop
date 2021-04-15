9149c4fd96aae476fc16a7df477d34eb148501f6
PrzemekBarczyk <przemek.barczyk@gmail.com>
2021-04-15 21:59:01 +0200

    Modified AuthorizationService
    Added new methods and modified old ones to use doctrine.

--- Backend/server/src/Service/AuthorizationService.php
+++ Backend/server/src/Service/AuthorizationService.php
@@ -1,61 +1,104 @@
 <?php
 namespace App\Service;
 
-use App\Repository\UserRepository;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 
 class AuthorizationService
 {
-    protected UserRepository $userRepository;
+    private EntityManagerInterface $em;
 
-    /**
-     * AuthorizationService constructor.
-     */
-    public function __construct()
+    public function __construct(EntityManagerInterface $em)
     {
-        $this->userRepository=new UserRepository();
+        $this->em = $em;
     }
 
-    public function login(string $email, string $password):boolean
+    public function checkLoginData(string $email, string $password): bool
     {
-        $user=$this->userRepository->getUserByEmail($email);
-        if(!$user)return false;
-        else {
-            $password===$user->getPassword();
+        $user = $this->getUserByEmailAndPassword($email, $password);
+        return $user == null ? false : true;
+    }
+
+    public function addUser(User $user): bool
+    {
+        if (!$this->checkIfUserExistsByEmail($user->getEmail())) // blocks creating users with same email
+        {
+            $this->em->persist($user);
+            $this->em->flush();
             return true;
         }
         return false;
-
     }
 
-    public function checkIfUserExists(string $email): boolean
+    public function addUserByDetails(string $name, string $surname, string $email,
+                                     string $password, bool $isAdmin): bool
     {
-        $user=$this->userRepository->getUserByEmail($email);
-        if(!$user)return true;
-        else return false;
-    }
-
-    public function addUser(string $name,string $surname, string $email, string $password, boolean $is_admin):boolean
-    {
-        $user = new User();
-        $user->setName($name)
+        if (!$this->checkIfUserExistsByEmail($email)) // blocks creating users with same email
+        {
+            $user = new User();
+            $user->setName($name)
                 ->setSurname($surname)
                 ->setEmail($email)
                 ->setPassword($password)
-                ->setIsAdmin($is_admin)
-                ->setCreatedAt(date("Y-m-d H:i:s"));
-        $this->userRepository->addUser($user);
+                ->setIsAdmin($isAdmin)
+                ->setCreatedAt(new \DateTime());
+            $this->addUser($user);
+            return true;
+        }
+        return false;
+    }
+
+    public function deleteUser(User $user): bool
+    {
+        if ($this->checkIfUserExistsById($user->getId()))
+        {
+            $this->em->remove($user);
+            $this->em->flush();
+            return true;
+        }
+        return false;
+    }
+
+    public function deleteUserById(int $id): bool
+    {
+        $user = $this->getUserById($id);
+        if ($user == null) return false;
+        $this->deleteUser($user);
         return true;
     }
 
-    public function deleteUserById(int $id): boolean
+    public function deleteUserByEmail(string $email)
     {
-        $user=$this->userRepository->getUserById($id);
-        if($user==NULL){
-            return false;
-        }
-        $this->userRepository->deleteUser($user);
+        $user = $this->getUserByEmail($email);
+        if ($user == null) return false;
+        $this->deleteUser($user);
         return true;
+    }
+
+    public function checkIfUserExistsById(int $id): bool
+    {
+        $user = $this->getUserById($id);
+        return $user == null ? false : true;
+    }
+
+    public function checkIfUserExistsByEmail(string $email): bool
+    {
+        $user = $this->getUserByEmail($email);
+        return $user == null ? false : true;
+    }
+
+    public function getUserById(int $id): ?User
+    {
+        return $this->em->getRepository(User::class)->find($id);
+    }
+
+    public function getUserByEmail(string $email): ?User
+    {
+        return $this->em->getRepository(User::class)->findOneBy(array('email'=>$email));
+    }
+
+    public function getUserByEmailAndPassword(string $email, string $password): ?User
+    {
+        return $this->em->getRepository(User::class)->findOneBy(array('email'=>$email, 'password'=>$password));
     }
 }
