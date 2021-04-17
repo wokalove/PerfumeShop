cc4581d28c31be4070632b635404f7a2239eca36
rarcz29 <rafalcz29@gmail.com>
2021-04-17 19:15:54 +0200

    Add auth actions

---
+++ Frontend/client/src/actions/authActions.js
@@ -0,0 +1,84 @@
+export const AUTH_ACTION_TYPES = {
+    LOADING = 'LOADING',
+    REGISTER_SUCCESS = 'REGISTER_SUCCESS',
+    REGISTER_FAIL = 'REGISTER_FAIL',
+    LOGIN_SUCCESS = 'LOGIN_SUCCESS',
+    LOGIN_FAIL = 'LOGIN_FAIL',
+    LOGOUT = 'LOGOUT',
+}
+
+// TODO: auth actions url's
+
+export const register = (username, email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: AUTH_ACTION_TYPES.LOADING,
+        });
+
+        await axios.post('', {
+            username,
+            email,
+            password,
+        });
+
+        dispatch({
+            type: AUTH_ACTION_TYPES.REGISTER_SUCCESS,
+        });
+    } catch (e) {
+        dispatch({
+            type: AUTH_ACTION_TYPES.REGISTER_FAIL,
+        });
+    }
+};
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: AUTH_ACTION_TYPES.LOADING,
+        });
+
+        const res = await axios
+            .post('', { usernameOrEmail, password })
+            .then((response) => {
+                if (response.data.token) {
+                    const userData = {
+                        token: response.data.token,
+                        refreshToken: 'asdfasdf',
+                    };
+
+                    localStorage.setItem(
+                        LOCAL_STORAGE.USER_DATA_NAME,
+                        JSON.stringify(userData)
+                    );
+                }
+
+                return response.data;
+            });
+
+        dispatch({
+            type: AUTH_ACTION_TYPES.LOGIN_SUCCESS,
+            payload: {
+                userId: 1, // TODO; user data
+                username: 'username',
+                email: 'email',
+                token: res.token,
+            },
+        });
+    } catch (e) {
+        dispatch({
+            type: AUTH_ACTION_TYPES.LOGIN_FAIL,
+        });
+    }
+};
+
+export const logout = () => (dispatch) => {
+    dispatch({
+        type: AUTH_ACTION_TYPES.LOADING,
+    });
+
+    localStorage.removeItem(LOCAL_STORAGE.USER_DATA_NAME);
+
+    dispatch({
+        type: AUTH_ACTION_TYPES.LOGOUT,
+    });
+};
