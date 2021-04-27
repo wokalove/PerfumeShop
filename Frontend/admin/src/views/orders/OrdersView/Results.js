9e9c966a7664553cefe3194cc04fbdf6dd0d26fc
Michał <michal.chlipala2@gmail.com>
2021-04-27 22:00:17 +0200

    add new page, view all orders

---
+++ Frontend/admin/src/views/orders/OrdersView/Results.js
@@ -0,0 +1,143 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import {
+  Box,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  makeStyles, Tooltip, TableSortLabel, TextField
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  avatar: {
+    marginRight: theme.spacing(2)
+  }
+}));
+
+const status = [
+  {
+    value: 'pending',
+    label: 'pending'
+  },
+  {
+    value: 'sent',
+    label: 'sent'
+  },
+  {
+    value: 'delivered',
+    label: 'delivered'
+  }
+];
+
+const Results = ({ className, orders, ...rest }) => {
+  const classes = useStyles();
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+  };
+
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  return (
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <PerfectScrollbar>
+        <Box minWidth={1050}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  Order Ref
+                </TableCell>
+                <TableCell>
+                  Customer
+                </TableCell>
+                <TableCell sortDirection="desc">
+                  <Tooltip
+                    enterDelay={300}
+                    title="Sort"
+                  >
+                    <TableSortLabel
+                      active
+                      direction="desc"
+                    >
+                      Date
+                    </TableSortLabel>
+                  </Tooltip>
+                </TableCell>
+                <TableCell>
+                  Status
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {orders.map((order) => (
+                <TableRow
+                  hover
+                  key={order.id}
+                >
+                  <TableCell>
+                    {order.ref}
+                  </TableCell>
+                  <TableCell>
+                    {order.customer.name}
+                  </TableCell>
+                  <TableCell>
+                    {moment(order.createdAt).format('DD/MM/YYYY')}
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      size="small"
+                      name="status"
+                      select
+                      SelectProps={{ native: true }}
+                      variant="outlined"
+                    >
+                      {status.map((option) => (
+                        <option
+                          key={option.status}
+                          value={option.status}
+                        >
+                          {option.label}
+                        </option>
+                      ))}
+                    </TextField>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </PerfectScrollbar>
+      <TablePagination
+        component="div"
+        count={orders.length}
+        onChangePage={handlePageChange}
+        onChangeRowsPerPage={handleLimitChange}
+        page={page}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Card>
+  );
+};
+
+Results.propTypes = {
+  className: PropTypes.string,
+  orders: PropTypes.array.isRequired
+};
+
+export default Results;
