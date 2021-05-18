099d646b014a4e81b49971ab033bb89200f5211b
rarcz29 <rafalcz29@gmail.com>
2021-05-18 23:19:52 +0200

    Update the summary component

--- Frontend/client/src/views/CartView/components/Summary/Summary.js
+++ Frontend/client/src/views/CartView/components/Summary/Summary.js
@@ -6,9 +6,6 @@ import styled from 'styled-components';
 const StyledContainer = styled.div`
   width: 300px;
   height: auto;
-  padding: 0.5rem;
-  background-color: black;
-  border-radius: 5px;
 `;
 
 const StyledHeader = styled.h1`
@@ -16,20 +13,16 @@ const StyledHeader = styled.h1`
   margin-top: 0.25rem;
   margin-bottom: 0.75rem;
   letter-spacing: 0.05rem;
-  color: white;
 `;
 
 const ContentContainer = styled.div`
   width: 100%;
-  background-color: white;
   margin-bottom: 0.5rem;
   padding-top: 0.5rem;
-  border-radius: inherit;
 `;
 
 const ContentElement = styled.div`
   height: 3rem;
-  margin: 0 1rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -73,9 +66,7 @@ const Summary = () => {
           <p>0</p>
         </Total>
       </ContentContainer>
-      <Button backgroundColor="white" color="black" width="100%">
-        Buy
-      </Button>
+      <Button width="100%">Buy</Button>
     </StyledContainer>
   );
 };
