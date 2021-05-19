030815929ecc60a6ed7450c265d1a85ac6b96102
Przemek <przemek.barczyk@gmail.com>
2021-05-19 17:48:08 +0200

    Update TransactionController.php
    Added methods to add and get transactions.

--- Backend/server/src/Controller/TransactionController.php
+++ Backend/server/src/Controller/TransactionController.php
@@ -2,25 +2,75 @@
 
 namespace App\Controller;
 
+use App\Service\ProductService;
+use App\Service\TransactionService;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TransactionController extends AbstractController
 {
-    /**
-     * @Route("/transactions", name="all_transactions")
-     */
-    public function allTransactions(): JsonResponse
+    private TransactionService $transactionService;
+    private ProductService $productService;
+    private UserService $userService;
+
+    public function __construct(TransactionService $transactionService,
+                                ProductService $productService,
+                                UserService $userService)
     {
-        return new JsonResponse("all transaction controller");
+        $this->transactionService = $transactionService;
+        $this->productService = $productService;
+        $this->userService = $userService;
     }
 
     /**
-     * @Route("/transactions/{limit}", name="transactions")
+     * @Route("/api/transactions", name="all_transactions", methods={"POST"})
      */
-    public function transactions(int $limit): JsonResponse
+    public function allTransactions(Request $request): JsonResponse
     {
-        return new JsonResponse("transactions with limit controller");
+        $transactions = json_decode($request->getContent(), true);
+
+        $userId = 1; // TODO: pobrać z tokenu
+        $user = $this->userService->getUserById($userId);
+
+        foreach ($transactions as $transaction)
+        {
+            $quantity = $transaction["quantity"];
+            $productId = $transaction["product_id"];
+
+            $product = $this->productService->getProductById($productId);
+            $productName = $product->getProductBase()->getName();
+            $price = $product->getPrice();
+
+            $this->transactionService->addTransactionByDetails($user, $product, $productName,
+                                                               $price, false, $quantity);
+        }
+        return $this->json(["message" => 'Transactions added successfully']);
+    }
+
+    /**
+     * @Route("/api/transactions", name="transactions", methods={"GET"})
+     */
+    public function transactions(Request $request): JsonResponse
+    {
+        $limit = $request->query->get("limit");
+        $transactions = $this->transactionService->getTransactions($limit);
+
+        return $this->json($this->arrayToJson($transactions));
+    }
+
+    function arrayToJson(Array $transactions): array
+    {
+        $response = array();
+        foreach ($transactions as $transaction)
+        {
+            $response[] = array(
+                'product_id' => $transaction->getProduct()->getId(),
+                'quantity' => $transaction->getQuantity()
+            );
+        }
+        return $response;
     }
 }
