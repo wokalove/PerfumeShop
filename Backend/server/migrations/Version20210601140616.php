6597c91707c01fb54cdf16a204db2b9990adb90b
wokalove <oladuda1999@gmail.com>
2021-06-01 16:18:36 +0200

    edit cascade in  Offer entity , remove old migrations and add new

--- Backend/server/migrations/Version20210601140616.php
+++ Backend/server/migrations/Version20210601140616.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210520124131 extends AbstractMigration
+final class Version20210601140616 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,11 @@ final class Version20210520124131 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE api_token_id_seq CASCADE');
+
         $this->addSql('DROP TABLE api_token');
         $this->addSql('ALTER TABLE transaction DROP CONSTRAINT fk_723705d167b3b43d');
         $this->addSql('DROP INDEX idx_723705d167b3b43d');
-        $this->addSql('ALTER TABLE transaction ADD usr_id INT NOT NULL');
-        $this->addSql('ALTER TABLE transaction DROP users_id');
+        $this->addSql('ALTER TABLE transaction RENAME COLUMN users_id TO usr_id');
         $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D1C69D3FB FOREIGN KEY (usr_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_723705D1C69D3FB ON transaction (usr_id)');
         $this->addSql('ALTER TABLE "user" ADD roles JSON NOT NULL');
@@ -38,7 +37,7 @@ final class Version20210520124131 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('CREATE SEQUENCE api_token_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+       // $this->addSql('CREATE SEQUENCE api_token_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE api_token (id INT NOT NULL, user_entity_id INT NOT NULL, token VARCHAR(255) NOT NULL, expires_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX idx_7ba2f5eb81c5f0b9 ON api_token (user_entity_id)');
         $this->addSql('ALTER TABLE api_token ADD CONSTRAINT fk_7ba2f5eb81c5f0b9 FOREIGN KEY (user_entity_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -48,8 +47,7 @@ final class Version20210520124131 extends AbstractMigration
         $this->addSql('ALTER TABLE "user" ALTER email TYPE VARCHAR(100)');
         $this->addSql('ALTER TABLE transaction DROP CONSTRAINT FK_723705D1C69D3FB');
         $this->addSql('DROP INDEX IDX_723705D1C69D3FB');
-        $this->addSql('ALTER TABLE transaction ADD users_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE transaction DROP usr_id');
+        $this->addSql('ALTER TABLE transaction RENAME COLUMN usr_id TO users_id');
         $this->addSql('ALTER TABLE transaction ADD CONSTRAINT fk_723705d167b3b43d FOREIGN KEY (users_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX idx_723705d167b3b43d ON transaction (users_id)');
     }
