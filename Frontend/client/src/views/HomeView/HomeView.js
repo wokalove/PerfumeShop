976d960eb1fe7441baf0c1f6e7138d0d373501d9
rarcz29 <rafalcz29@gmail.com>
2021-06-04 21:41:39 +0200

    client: load special offers

--- Frontend/client/src/views/HomeView/HomeView.js
+++ Frontend/client/src/views/HomeView/HomeView.js
@@ -1,107 +1,67 @@
 import '@brainhubeu/react-carousel/lib/style.css';
 import backgroundImage from 'assets/home-background.jpg';
 import image from 'assets/pngegg.png';
+import axios from 'axiosConfig';
 import Container from 'components/common/Container';
 import ShopItem from 'components/ShopItem';
 import DIMENSIONS from 'constants/dimensions';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 import CustomCarousel from './components/CustomCarousel';
 import HomeSection from './components/HomeSection';
-import { HomeImageWrapper, RedSquare, StyledImg } from './styles';
+import { HomeImageWrapper, StyledImg } from './styles';
 
 const HomeView = () => {
-    const [value, setValue] = useState(0);
-    const [slides, setSlides] = useState([
-        <RedSquare />,
-        <RedSquare />,
-        <RedSquare />,
-        <RedSquare />,
-        <RedSquare />,
-        <RedSquare />,
-        <RedSquare />,
-        <RedSquare />,
-    ]);
+  const [value, setValue] = useState(0);
+  const [productsWithOffers, setProductsWithOffers] = useState([]);
 
-    const handleChange = (value) => {
-        setValue(value);
+  useEffect(() => {
+    const loadProductsWithSpecialOffers = async () => {
+      const res = await axios.get('products?offer=true');
+      setProductsWithOffers(res.data);
     };
+    loadProductsWithSpecialOffers();
+  }, []);
 
-    return (
-        <>
-            <HomeImageWrapper>
-                <ParallaxProvider>
-                    <Parallax y={[-50, 50]} tagOuter="figure">
-                        <StyledImg src={backgroundImage} alt="img" />
-                    </Parallax>
-                </ParallaxProvider>
-            </HomeImageWrapper>
-            <Container maxWidth={DIMENSIONS.PAGE_WIDTH + 'px'}>
-                <main>
-                    <HomeSection title="New Products">
-                        <CustomCarousel>
-                            {/* //{slides.map((item, index) => item)} */}
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                        </CustomCarousel>
-                    </HomeSection>
-                    <HomeSection title="Special Offers">
-                        <CustomCarousel>
-                            {/* //{slides.map((item, index) => item)} */}
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                            <ShopItem
-                                theBigOne
-                                imageSrc={image}
-                                price="59.99"
-                            />
-                        </CustomCarousel>
-                    </HomeSection>
-                </main>
-            </Container>
-        </>
-    );
+  console.log(productsWithOffers);
+
+  const handleChange = (value) => {
+    setValue(value);
+  };
+
+  return (
+    <>
+      <HomeImageWrapper>
+        <ParallaxProvider>
+          <Parallax y={[-50, 50]} tagOuter="figure">
+            <StyledImg src={backgroundImage} alt="img" />
+          </Parallax>
+        </ParallaxProvider>
+      </HomeImageWrapper>
+      <Container maxWidth={DIMENSIONS.PAGE_WIDTH + 'px'}>
+        <main>
+          <HomeSection title="New Products">
+            <CustomCarousel>
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+            </CustomCarousel>
+          </HomeSection>
+          <HomeSection title="Special Offers">
+            <CustomCarousel>
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+              <ShopItem theBigOne imageSrc={image} price="59.99" />
+            </CustomCarousel>
+          </HomeSection>
+        </main>
+      </Container>
+    </>
+  );
 };
 
 export default HomeView;
